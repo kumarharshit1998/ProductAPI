d26819a19ca2fde2e2d81ea67ef9e321e3c78880
kumarharshit1998 <44222168+kumarharshit1998@users.noreply.github.com>
2021-04-01 19:22:56 +0530

    Add files via upload

---
+++ ClientMVC/Controllers/ProductController.cs
@@ -0,0 +1,139 @@
+﻿using ClientMVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public async Task<ActionResult> IndexAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44355/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("api/Product/");
+                if (response.IsSuccessStatusCode)
+                {
+                    var jasonString = response.Content.ReadAsStringAsync();
+                    jasonString.Wait();
+                    var ProductList = JsonConvert.DeserializeObject<List<Product>>(jasonString.Result);
+                    return View(ProductList);
+                }
+                return null;
+            }
+        }
+
+        public async Task<ActionResult> GetProductById(int? id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44355/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("api/Product/" + id);
+                if (response.IsSuccessStatusCode)
+                {
+                    Product product = await response.Content.ReadAsAsync<Product>();
+                    return View(product);
+                }
+                return RedirectToAction("IndexAsync");
+            }
+        }
+        public async Task<ActionResult> Insert()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Insert(Product product)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44355/");
+                HttpResponseMessage response = await client.PostAsJsonAsync("api/Product", product);
+                if (response.IsSuccessStatusCode)
+                {
+
+                    return RedirectToAction("IndexAsync");
+                }
+            }
+            return View();
+        }
+        public async Task<ActionResult> Edit(int? id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44355/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("api/Product/" + id);
+                if (response.IsSuccessStatusCode)
+                {
+                    Product product = await response.Content.ReadAsAsync<Product>();
+                    return View(product);
+                }
+            }
+            return RedirectToAction("IndexAsync");
+        }
+        [HttpPost]
+        public async Task<ActionResult> Edit(int? id, Product product)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44355/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.PutAsJsonAsync("api/Product/" + id, product);
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    return RedirectToAction("IndexAsync");
+                }
+            }
+            return View();
+        }
+        public async Task<ActionResult> Delete(int? id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44355/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("api/Product/" + id);
+                if (response.IsSuccessStatusCode)
+                {
+                    Product product = await response.Content.ReadAsAsync<Product>();
+                    return View(product);
+                }
+            }
+            return RedirectToAction("IndexAsync");
+        }
+        [HttpPost]
+        public async Task<ActionResult> Delete(int? id, Product product)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44355/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                HttpResponseMessage response = await client.DeleteAsync("api/Product/" + id);
+                if (response.IsSuccessStatusCode)
+                {
+
+                    return RedirectToAction("IndexAsync");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ ClientMVC/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientMVC.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int QtyInStock { get; set; }
+
+        public string Description { get; set; }
+
+        public string Supplier { get; set; }
+    }
+}
